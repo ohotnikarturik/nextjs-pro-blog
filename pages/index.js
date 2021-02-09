50e9cfab1e47f33ad247b5f8bae4e34bbe3e0ec3
Okhotnichenko Artur <ohotnikarturik@gmail.com>
2021-02-09 11:20:06 +0200

    edit tailwind config, set up eslint and prettier config

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,21 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-500">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>Hello</div>
+      <div class="my-10 p-6 max-w-sm mx-auto h-300 bg-white rounded shadow-md flex-col justify-between items-center space-x-4">
+        <div class="flex-shrink-0">
+          <img class="h-12 w-12" src="/vercel.svg" alt="ChitChat Logo" />
+        </div>
+        <div>
+          <div className="text-xl font-medium text-primary-red">ChitChat</div>
+          <p class=" text-primary-purple font-mono">You have a new message!</p>
+        </div>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          red: "#F67280",
+          black: "#152135",
+          white: "#FFFFFF",
+          gray: "#828282",
+          pink: "#FFB7B7",
+          purple: "#DC78FF",
+          crimson: "#DC143C",
+        },
+      },
+      fontFamily: {
+        body: ["Nunito"],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
