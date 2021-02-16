cee35953ad49517e0f544139e246f103671b65c8
Okhotnichenko Artur <ohotnikarturik@gmail.com>
2021-02-16 08:56:57 +0200

    create 404 page not found comp, add t0 404 page, add yarn start script to build on package.json

--- components/PostCardItem.tsx
+++ components/PostCardItem.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 import Badge from './Badge'
 import Button from './Button'
@@ -7,59 +8,65 @@ import { PostCardItemProps } from '../interfaces'
 
 const PostCardItem = ({ post }: any) => {
   return (
-    <>
-      <div className=" bg-primary-white overflow-hidden rounded-md">
-        <div className="h-1/2 relative shadow-sm">
-          <Image
-            src={'https:' + post.fields.postImg.fields.file.url}
-            layout="responsive"
-            width={385}
-            height={285}
-            quality={100}
-            objectFit={'cover'}
-            alt="Post image"
-          />
-          <div className="absolute left-0 bottom-0">
-            <Badge
-              color={post.fields.badgeColor}
-              label={post.fields.category}
+    <Link href="post/[id]" as={`post/${post.sys.id}`}>
+      <a>
+        <div className=" bg-primary-white overflow-hidden rounded-md">
+          <div className="h-1/2 relative shadow-sm">
+            <Image
+              src={'https:' + post.fields.postImg.fields.file.url}
+              layout="responsive"
+              width={385}
+              height={285}
+              quality={100}
+              objectFit={'cover'}
+              alt="Post image"
             />
-          </div>
-        </div>
-        <div className="h-1/2 px-4 pt-4 pb-7">
-          <div className="h-full">
-            <div className="overflow-scroll h-7">
-              <Subtitle boldStyle label={post.fields.title} />
-            </div>
-            <div className="h-20 mt-3 max-h-20 overflow-scroll">
-              <span>{post.fields.excerpt}</span>
-            </div>
-          </div>
-          <div className="flex items-center justify-between pt-4">
-            <div className="flex items-center mb-2 s:mb-0">
-              <Image
-                className=" rounded-full"
-                objectFit={'cover'}
-                src={'https:' + post.fields.authorImg.fields.file.url}
-                alt="Profile"
-                width={30}
-                height={30}
-                quality={100}
+            <div className="absolute left-0 bottom-0">
+              <Badge
+                color={post.fields.badgeColor}
+                label={post.fields.category}
               />
-              <span className="ml-3 font-medium text-sm">
-                {post.fields.authorName}
-              </span>
             </div>
-            <div>
-              <span className="text-xs font-medium">{post.fields.date}</span>
+          </div>
+          <div className="h-1/2 px-4 pt-4 pb-7">
+            <div className="h-full">
+              <div className="overflow-scroll h-7">
+                <Subtitle boldStyle label={post.fields.title} />
+              </div>
+              <div className="h-20 mt-3 max-h-20 overflow-scroll">
+                <span>{post.fields.excerpt}</span>
+              </div>
+            </div>
+            <div className="flex items-center justify-between pt-4">
+              <div className="flex items-center mb-2 s:mb-0">
+                <Image
+                  className=" rounded-full"
+                  objectFit={'cover'}
+                  src={'https:' + post.fields.authorImg.fields.file.url}
+                  alt="Profile"
+                  width={30}
+                  height={30}
+                  quality={100}
+                />
+                <span className="ml-3 font-medium text-sm">
+                  {post.fields.authorName}
+                </span>
+              </div>
+              <div>
+                <span className="text-xs font-medium">{post.fields.date}</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="absolute right-3 -bottom-5">
-        <Button label="Read more" path="post/[id]" as={`post/${post.sys.id}`} />
-      </div>
-    </>
+        <div className="absolute right-3 -bottom-5">
+          <Button
+            label="Read more"
+            path="post/[id]"
+            as={`post/${post.sys.id}`}
+          />
+        </div>
+      </a>
+    </Link>
   )
 }
 
--- pages/404.tsx
+++ pages/404.tsx
@@ -1,42 +1,7 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import NotFoundPage404 from '../components/NotFoundPage404'
 
-import Meta from '../components/Meta'
-
-const notFoundPage404 = () => {
-  const router = useRouter()
-
-  return (
-    <>
-      <Meta
-        title="ProBlog App | 404 not found page"
-        description="Not found page"
-        keywords="404"
-      />
-      <div className="flex flex-col justify-start items-center">
-        <h2 className="text-9xl text-primary-red font-bold border-primary-red border-b">
-          404
-        </h2>
-        <span className="font-medium text-primary-black mt-6">
-          This page could not be found. Go{' '}
-          <button
-            className="focus:outline-none hover:text-primary-crimson hover:border-primary-crimson transition ease-in duration-200"
-            onClick={() => router.back()}
-          >
-            <a className="text-primary-red border-b border-primary-red hover:text-primary-crimson hover:border-primary-crimson transition ease-in duration-200">
-              Back
-            </a>
-          </button>
-          <span> or</span>{' '}
-          <Link href="/">
-            <a className="text-primary-red border-b border-primary-red hover:text-primary-crimson hover:border-primary-crimson transition ease-in duration-200">
-              Home
-            </a>
-          </Link>
-        </span>
-      </div>
-    </>
-  )
+const page404 = () => {
+  return <NotFoundPage404 />
 }
 
-export default notFoundPage404
+export default page404
