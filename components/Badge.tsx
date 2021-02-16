a79eac1431dbc6dfc0d48465f64e1f7adf1b1621
Okhotnichenko Artur <ohotnikarturik@gmail.com>
2021-02-16 02:39:26 +0200

    test badge bg

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -5,7 +5,7 @@ const Badge = ({ label, color, postStyle }: BadgeProps) => {
 
   return (
     <div
-      className={`bg-primary-${color} text-center tracking-wide w-32 py-2 px-2 inline-block ${
+      className={`bg-primary-red text-center tracking-wide w-32 py-2 px-2 inline-block ${
         postStyle ? 'rounded-tl-md rounded-br-md' : 'rounded-tr-md'
       } text-sm font-semibold tl-md text-primary-white`}
     >
