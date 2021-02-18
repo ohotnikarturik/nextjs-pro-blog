c575b9907b4fc5523864817a8891e36c55c01e41
Okhotnichenko Artur <ohotnikarturik@gmail.com>
2021-02-18 09:34:18 +0200

    changes in next.config, readme, manifest

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![pas](https://img.shields.io/static/v1?&message=ProgressiveApp.Store&color=74b9ff&style=flat&label=Follow%20ProBlog%20at)](https://progressiveapp.store/pwa/ProBlog)
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+// const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   future: { webpack5: true },
   pwa: {
     disable: process.env.NODE_ENV === 'development',
     dest: 'public',
-    runtimeCaching,
+    // runtimeCaching,
   },
   images: {
     domains: ['images.ctfassets.net'],
